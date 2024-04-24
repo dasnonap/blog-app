f4108a69e22446e130265c3afa439d0f5702a3ea
Ivan Mihov <53471141+dasnonap@users.noreply.github.com>
2024-04-24 22:10:18 +0300

    Feat(models): Add username in User model (#4)
    * Append username column to user
    
    * Add username in controllers
    
    * Update username user tests and userfactory
    
    * Add sessions table
    
    * feat(user_model)Add username field for User register form

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,14 +33,18 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => 'required|string|max:255',
-            'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'email' => 'required|string|lowercase|email|max:255|unique:' . User::class,
+            'password' => [
+                'required', 'confirmed', Rules\Password::defaults(),
+            ],
+            'username' => 'required|string|max:255'
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'username' => $request->username,
         ]);
 
         event(new Registered($user));
